0fde8e9846f0e4e1f876facb9724dec4d7c35f28
Etettttt <112492696+Etettttt@users.noreply.github.com>
2022-11-06 23:29:17 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '360px',
+      md: '720px',
+      lg: '900px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        darkPurple: '#232946',
+        lightPurple: '#b8c1ec',
+        offPink: '#eebbc3',
+        offWhite: '#e9ecef',
+        darkGray: '212529',
+      },
+    },
+  },
+  plugins: [],
+};
